32c20669cbeadc914ebcd2515a237026e47c9031
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-10-15 15:04:58 +0200

    Creating user menu and moving logout link there

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import { fetchUserAttributes } from 'aws-amplify/auth';
 import { useState } from "react";
 import appMenuIcon from "./assets/images/menu/appMenu.png?url";
+import userMenuIcon from "./assets/images/menu/userMenu.png?url";
 
 function App() {
 
     const [userNickname, setUserNickname] = useState("")
     const [userNicknameFetchInProgress, setUserNicknameFetchInProgress] = useState(false)
     const [appMenuExpanded, setAppMenuExpanded] = useState(false)
+    const [userMenuExpanded, setUserMenuExpanded] = useState(false)
 
     function getUserNickname() {
         setUserNicknameFetchInProgress(true)
@@ -41,7 +43,22 @@ function App() {
             </div>
         } else {
             return <div className="appMenu">
-                <img src={appMenuIcon} alt="menu" onClick={() => setAppMenuExpanded(true)}/>
+                <img src={appMenuIcon} alt="menu" onClick={() => { setAppMenuExpanded(true); setUserMenuExpanded(false)}}/>
+            </div>
+        }
+    }
+
+    function UserMenu({ signoutFunction }: { signoutFunction?: (data?: any) => void }) {
+        if (userMenuExpanded) {
+            return <div className="userMenu">
+                <img src={userMenuIcon} alt="menu" onClick={() => setUserMenuExpanded(false)}/>
+                <ul>
+                    <li><a onClick={() => signoutFunction?.()}>Wyloguj</a></li>
+                </ul>
+            </div>
+        } else {
+            return <div className="userMenu">
+                <img src={userMenuIcon} alt="menu" onClick={() => { setUserMenuExpanded(true); setAppMenuExpanded(false)}}/>
             </div>
         }
     }
@@ -49,15 +66,17 @@ function App() {
     return (
         <Authenticator>
             {({ signOut }) => {
+                console.log(typeof signOut);
                 return ( 
                     <main>
                         <AppMenu/>
+                        <UserMenu signoutFunction={signOut}/>
                         <h1>Chores Cooperative</h1>
                         <p className="versionInfo">Wersja 0.3.1</p>
 
                         <div className="subheader">
                             <p className="userInfo">Witaj, {userNickname}</p>
-                            <a className="logoutButton" onClick={signOut}>Wyloguj</a>
+                            
                         </div>
                         <div style={{clear: 'both'}}/>
                         <BrowserRouter>
