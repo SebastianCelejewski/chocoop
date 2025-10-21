ec26a615824c9ccb514c269376e475e847522c58
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-10-21 12:43:02 +0200

    Adding functionality to Lambda function to refresh exp stats for users

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,6 +3,7 @@ import { auth } from './auth/resource';
 import { data } from './data/resource';
 
 import { Stack } from "aws-cdk-lib";
+import * as cdk from "aws-cdk-lib";
 import { Policy, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
 import { StartingPosition, EventSourceMapping } from "aws-cdk-lib/aws-lambda";
 import { myDynamoDBFunction } from "./functions/dynamoDB-function/resource";
@@ -57,3 +58,47 @@ const mapping = new EventSourceMapping(
 );
 
 mapping.node.addDependency(policy);
+
+/* SSM parameter holding a name of DynamiDB table that can be fetched by the Lambda function */
+
+const envName = process.env.AMPLIFY_BRANCH || 'dev';
+const activityTableParam = new cdk.aws_ssm.StringParameter(
+    Stack.of(activityTable),
+    "ActivityTableName",
+    {
+        parameterName: `/pl.sebcel.chocoop/${envName}/activity-table-name`,
+        stringValue: activityTable.tableName,
+    }
+);
+
+const allowSignLambdaToAccessAccountTablePolicy = new Policy(
+    Stack.of(activityTable),
+    "chocoop-allowSignLambdaToAccessActivityTablePolicy",
+    {
+        statements: [
+        new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "dynamodb:GetItem",
+                "dynamodb:UpdateItem",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+            ],
+            resources: [activityTable.tableArn],
+        }),
+        new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                "ssm:GetParameter",
+                "ssm:GetParameters",
+                "ssm:GetParametersByPath"
+            ],
+            resources: [
+                activityTableParam.parameterArn,
+            ],
+        }),
+        ],
+    }
+);
+
+backend.myDynamoDBFunction.resources.lambda.role?.attachInlinePolicy(allowSignLambdaToAccessAccountTablePolicy);
--- amplify/functions/dynamoDB-function/handler.ts
+++ amplify/functions/dynamoDB-function/handler.ts
@@ -1,26 +1,63 @@
-import type { DynamoDBStreamHandler } from "aws-lambda";
+import type { DynamoDBStreamHandler, DynamoDBStreamEvent } from "aws-lambda";
 import { Logger } from "@aws-lambda-powertools/logger";
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { DynamoDBClient, ScanCommand } from "@aws-sdk/client-dynamodb";
+
+const ssmClient = new SSMClient();
+const dynamoDbClient = new DynamoDBClient({});
+const envName = process.env.AMPLIFY_BRANCH || 'dev';
 
 const logger = new Logger({
     logLevel: "INFO",
     serviceName: "dynamodb-stream-handler",
 });
 
-export const handler: DynamoDBStreamHandler = async (event) => {
-    logger.info("Entering dynamoDB function handlerr")
-    logger.info(JSON.stringify(event))
+async function getParameter(name: string): Promise<string> {
+    const paramPath = `/secure-site/${envName}/${name}`;
+    const command = new GetParameterCommand({ Name: paramPath });
+    const response = await ssmClient.send(command);
+    return response.Parameter?.Value || '';
+}
+
+const logDataChange = async (event: DynamoDBStreamEvent) => {
+    logger.info("Logging the data change");
     for (const record of event.Records) {
         logger.info(`Processing record: ${record.eventID}`);
         logger.info(`Event Type: ${record.eventName}`);
-
-        if (record.eventName === "INSERT") {
-            // business logic to process new records
-            logger.info(`New Image: ${JSON.stringify(record.dynamodb?.NewImage)}`);
-          }
+        logger.info(JSON.stringify(record))
     }
     logger.info(`Successfully processed ${event.Records.length} records.`);
+}
 
+const rebuildStatistics = async () => {
+    logger.info("Rebuilding statistics")
+    const tableName = await getParameter('account-table-name');
+    logger.info(`Table name: ${tableName}`)
+
+    const params = {
+        TableName: tableName,
+        ExclusiveStartKey: undefined as any
+    };
+    let items;
+    do{
+        const command = new ScanCommand(params);
+        items = await dynamoDbClient.send(command);
+        items.Items?.forEach((item: object) => {
+          logger.info(JSON.stringify(item))
+        });
+        params.ExclusiveStartKey = items.LastEvaluatedKey;
+    }while(typeof items.LastEvaluatedKey !== "undefined");
+    
+    logger.info("Rebuilding statistics completed")
+}
+
+export const handler: DynamoDBStreamHandler = async (event) => {
+    logger.info("Entering dynamoDB function handler")
+    await logDataChange(event)
+    await rebuildStatistics()
+
+    logger.info("Exiting dynamoDB function handler")
     return {
-        batchItemFailures: [],
+        batchItemFailures: []
     };
 };
