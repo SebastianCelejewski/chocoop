019a8daadb31c2fef82a82035692bb91a786d847
Sebastian Celejewski <Sebastian.Celejewski@wp.pl>
2025-11-10 15:35:37 +0100

    Enabling virtualized list for activities

--- src/pages/activities/activityList.tsx
+++ src/pages/activities/activityList.tsx
@@ -1,13 +1,19 @@
 import type { Schema } from "../../../amplify/data/resource";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { generateClient } from "aws-amplify/data";
 import { dateToString } from "../../utils/dateUtils";
+import { List, AutoSizer, CellMeasurer, CellMeasurerCache } from 'react-virtualized';
 
 import User from "../../model/User";
 import { ReactionsFromAllUsers } from "../../components/reactions";
 
 const client = generateClient<Schema>();
+
+const cache = new CellMeasurerCache({
+    fixedWidth: true,
+    defaultHeight: 100
+});
 
 class ActivityQueryResult {
   items: Array<Schema["Activity"]["type"]> = []
@@ -23,6 +29,8 @@ function ActivityList({users}: {users: Map<string, User>}) {
     const [reactions, setReactions] = useState<Array<Schema["Reaction"]["type"]>>([]);
 
     const navigate = useNavigate();
+
+    cache.clearAll()
     
     useEffect(() => {
         const activitiesQuery = client.models.Activity.observeQuery().subscribe({
@@ -63,31 +71,56 @@ function ActivityList({users}: {users: Map<string, User>}) {
             <p className="pageTitle" onClick={navigateToWorkRequests}>Lista wykonanych czynności</p>
             <div className="loadingData">Ładowanie danych</div>
             <div className="buttonPanel">
-              <button onClick={createActivity}>Dodaj czynność</button>
+                <button onClick={createActivity}>Dodaj czynność</button>
             </div>
         </>
     }
 
-    return (
-        <>
-            <p className="pageTitle" onClick={navigateToWorkRequests}>Lista wykonanych czynności</p>
-            <ul className="entityList">
-            {activities.map(activity => {
-                return <li
-                        className="entityListElement"
-                        onClick={() => showActivity(activity.id)}
-                        key={activity.id}>
-                        <div>
+    function renderRow({ index, key, style, parent }: { index: number, key: string, style: React.CSSProperties, parent: any}) {
+        const activity = activities[index];
+        return (
+            <CellMeasurer
+                key={key}
+                cache={cache}
+                parent={parent}
+                columnIndex={0}
+                rowIndex={index}>
+                {({registerChild}) => (
+                    <div style={style} className="row" ref={registerChild}>
+                        <li
+                            className="entityListElement"
+                            onClick={() => showActivity(activity.id)}
+                            key={activity.id}>
                             <p className="entityDateTime">{dateToString(activity.dateTime)}</p>
                             <p className="entityPerson">{users.get(activity.user)?.nickname}</p>
                             <p className="entityType">{activity.type}</p>
                             <p className="entityExp">{activity.exp} xp</p>
                             <div style={{clear: 'both'}}/>
                             <ReactionsFromAllUsers activityId={activity.id} reactions={reactions}/>
-                        </div>
-                      </li>
+                        </li>
+                    </div>
+                )}
+            </CellMeasurer>
+        );
+    }
+
+    return (
+        <>
+            <p className="pageTitle" onClick={navigateToWorkRequests}>Lista wykonanych czynności</p>
+            <ul className="entityList">
+                <AutoSizer>
+                {
+                    ({ width, height }) => (<List
+                        width={width}
+                        height={height}
+                        deferredMeasurementCache={cache}
+                        rowHeight={cache.rowHeight}
+                        rowRenderer={renderRow}
+                        rowCount={activities.length}
+                        overscanRowCount={3} />
+                    )
                 }
-            )}
+                </AutoSizer>
             </ul>
             <div className="buttonPanel">
                 <button onClick={createActivity}>Dodaj czynność</button>
